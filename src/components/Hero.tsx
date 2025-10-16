0154fefc49896d53987d95970247e71d83c10282
SidhardhCR <sidhardhcr007@gmail.com>
2025-10-17 01:00:59 +0530

    updated 10

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'motion/react';
 import { Button } from './ui/button';
 import { ImageWithFallback } from './figma/ImageWithFallback';
-import { useNavigate } from 'react-router-dom';
+
 export function Hero() {
   const scrollToSection = (id: string) => {
     const element = document.getElementById(id);
@@ -9,7 +9,7 @@ export function Hero() {
       element.scrollIntoView({ behavior: 'smooth' });
     }
   };
-  const navigate = useNavigate();
+
 
   return (
     <section id="home" className="relative min-h-screen flex items-center justify-center overflow-hidden">
@@ -64,10 +64,10 @@ export function Hero() {
           <Button
             size="lg"
             variant="outline"
-            onClick={() => navigate('/form')}
+            onClick={() => scrollToSection('contact')}
             className="border-white text-white hover:text-white hover:bg-[#9d33c4]  px-8 py-6 text-lg"
           >
-            Book Consultation
+            Book a free 30 min discovery session
           </Button>
         </motion.div>
       </div>
